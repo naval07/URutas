bd66cf043cb83bd8a7319e4b9a5b189841ed657a
Emanuel <emanuelnaval07@gmail.com>
2024-04-25 18:21:07 -0500

    Avance diseño responsive - NavBar y pag advaced search

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import React from 'react';
-import './App.css';
-import {useState} from "react";
-import Header from "./components/header/header"
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/header/header";
 import Subheader from "./components/subheader/Subheader";
-import Catalogo from './components/catalogo/Catalogo';
+import Catalogo from "./components/catalogo/Catalogo";
 import Searchbar from "./components/searchBar/searchbar";
 
-
 import catalogo from "./components/catalogo/Catalogo";
-import Selection from "./components/checklist/Selection"
+import Selection from "./components/checklist/Selection";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import cursos from "./cursos.json";
 
-import Home from './pages/home/home';
-import Search from './pages/search/search';
-import AdvancedSearch from './pages/advancedSearch/AdvancedSearch';
+import Home from "./pages/home/home";
+import Search from "./pages/search/search";
+import AdvancedSearch from "./pages/advancedSearch/AdvancedSearch";
 
 function App() {
-
-    return (
-        <div className="container-fluid overflow-hidden">
-
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/search' element={<Search/>}/>
-                    <Route path='/advancedSearch' element={<AdvancedSearch/>}/>
-                </Routes>
-            </BrowserRouter> 
-            
-
-        </div>
-    );
+  return (
+    // <div className="container-fluid overflow-hidden">
+    // Quitamos las clases para quitar el padding y el margin de la página principal
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/advancedSearch" element={<AdvancedSearch />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,34 +1,61 @@
-import React from 'react';
-import './header.css';
-import LogoUR from './thumbnail_UROSARIO_Logo_Horizontal_nuevocolor.png';
-import iconBuscar from './icon_busqueda.svg';
+import React from "react";
+import "./header.css";
+import LogoUR from "./thumbnail_UROSARIO_Logo_Horizontal_nuevocolor.png";
+import iconBuscar from "./icon_busqueda.svg";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Header(){
-    return(
-        /*caracteristicas del componente*/
-        <header>
-            <div className = "logoUR">
-                <img src={LogoUR} alt=''></img>
-            </div>
-            <div className = "nav">
-                <Link to='/'>
-                    Inicio
-                </Link>
-                <Link to='/search'>
-                    Búsqueda por aptitud
-                </Link>
-                <Link to='/advancedSearch'>
-                    Búsqueda específica
-            </Link>
-            </div>
-            <div className="search">
-            <input type="text" id="searchInput" placeholder="Buscar..."/>
-            <button id="searchButton"><img src={iconBuscar}/></button>
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"; // npm install react-icons
+
+function Header() {
+  // Referencia del navbar
+  const navRef = useRef();
+
+  // Funcion para el menú desplegable
+  const mostrarMenu = () => {
+    navRef.current.classList.toggle("navbar-burger");
+  };
+
+  return (
+    /*caracteristicas del componente*/
+    <header>
+      <div className="logoUR">
+        <Link to="/">
+          <img src={LogoUR} alt=""></img>
+        </Link>
+      </div>
+      {/* Definir la referencia en la navbar */}
+      <div className="nav" ref={navRef}>
+        <div className="nav-element" id="nav-elem-inicio">
+          <Link to="/">Inicio</Link>
         </div>
-        </header>
-    )
+        <div className="nav-element" id="nav-elem-aptitud">
+          <Link to="/search">Búsqueda por aptitud</Link>
+        </div>
+        <div className="nav-element" id="nav-elem-especifica">
+          <Link to="/advancedSearch">Búsqueda específica</Link>
+        </div>
+        <button class="nav-btn nav-close-btn" onClick={mostrarMenu}>
+          <FaTimes />
+        </button>
+      </div>
+      <button class="nav-btn" onClick={mostrarMenu}>
+        <FaBars />
+      </button>
+      {/* <div className="search">
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Buscar..."
+          ref={inputRef}
+        />
+        <button id="searchButton">
+          <img src={iconBuscar} />
+        </button>
+      </div> */}
+    </header>
+  );
 }
 
 export default Header;
