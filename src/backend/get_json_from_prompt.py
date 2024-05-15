9b5de2a7a8cd8cd11a7eafebd8ce983819a97475
LauraOrtiz-235 <lauraso.ortiz@urosario.edu.co>
2024-05-15 12:14:26 -0500

    cambios del modelo

--- src/backend/get_json_from_prompt.py
+++ src/backend/get_json_from_prompt.py
@@ -8,38 +8,6 @@ import os
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
-"""
-def get_json_from_prompt(prompt, dataset):
-    nlp = spacy.load("es_core_news_sm")
-    prompt = nlp(prompt)
-    selected_courses = get_list_from_prompt(dataset, prompt, nlp)
-    route = get_route_from_list(selected_courses, custom_sort_key)
-
-    lista_de_diccionarios = []
-    for curso in route:
-        curso_dict = {
-            "id": curso[0],
-            "titulo": curso[1],
-            "descripcion": curso[2],
-            "area": curso[3]
-        }
-        lista_de_diccionarios.append(curso_dict)
-
-    json_data = json.dumps(lista_de_diccionarios, ensure_ascii=False, indent=2)
-    #ruta_archivo = "../src/catalogo.json"
-    ruta_archivo = "../cursos.json"
-
-    ruta_completa = os.path.abspath(ruta_archivo)
-    print('RUTAA', ruta_completa)
-    with open(ruta_completa, "w", encoding='utf-8') as json_file:
-           json.dump(lista_de_diccionarios, json_file, ensure_ascii=False, indent=2)
-
-    # print(json_data)
-    # print('primero', json_data[0])
-    json_data = '{' + json_data[1:len(json_data)-1] + '}'
-    #print(json_data)
-    return json_data
-"""
 def get_json_from_prompt(prompt, dataset):
     model_name = "MoritzLaurer/multilingual-MiniLMv2-L6-mnli-xnli"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
